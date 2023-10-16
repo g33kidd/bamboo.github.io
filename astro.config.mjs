b4a2b5ccfc37da78ba93deeafc51cba219701dd6
g33k <kidd.josh.343@gmail.com>
2023-10-15 19:21:25 -0600

    Added some initial pages. Setup github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://bamboo.github.io',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Bamboo Docs',
+			tagline: 'Web Framework for Bun.sh',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/g33kidd/bamboo',
 			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			// TODO: Update this when more pages are written
+			// sidebar: [
+			// 	{
+			// 		label: 'Guides',
+			// 		items: [
+			// 			// Each item here is one entry in the navigation menu.
+			// 			{label: 'Getting Started', link: '/guides/getting-started'},
+			// 			{ label: 'Example Guide', link: '/guides/example/' },
+			// 		],
+			// 	},
+			// 	{
+			// 		label: 'Reference',
+			// 		autogenerate: { directory: 'reference' },
+			// 	},
+			// ],
 		}),
 	],
 });
---
+++ src/content/docs/guides/getting-started.md
@@ -0,0 +1,10 @@
+---
+title: Getting Started
+description: A quick-look into using Bamboo.
+---
+
+Bamboo is a modern Web Framework for [Bun](https://bun.sh). This documentation is currently a work in progress.
+
+# Installation
+
+# Project Setup
